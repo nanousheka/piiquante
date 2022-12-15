1b03bffb878402373ecbd0c7c04449c69f5063b3
nanousheka <ineszocly@gmail.com>
2022-12-15 14:42:02 -0400

    Add validation (min 1 & max 10) to heat number

--- backend/models/Sauce.js
+++ backend/models/Sauce.js
@@ -7,7 +7,7 @@ const sauceSchema = mongoose.Schema({
   description: { type: String, required: true },
   mainPepper : {type: String, required: true},
   imageUrl: { type: String, required: true },
-  heat: { type: Number, required: true },
+  heat: { type: Number, min : 1, max : 10, required: true },
   likes: { type: Number, required: false },
   dislikes: { type: Number, required: false },
   usersLiked: {type: ["String <userId>"], required: false},
