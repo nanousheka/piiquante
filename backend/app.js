c785f272f2ceb6b87e39870911b274b41b88fe28
nanousheka <ineszocly@gmail.com>
2022-12-15 17:20:31 -0400

    Add Static middleware and improve images paths

--- backend/app.js
+++ backend/app.js
@@ -27,6 +27,7 @@ app.use((req, res, next) => {
 
 
 app.use(bodyParser.json());
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 //Router modules
 const sauceRoutes = require('./routes/sauce');
@@ -38,3 +39,5 @@ app.use('/api/auth', userRoutes);
 
 
 module.exports = app;
+
+
