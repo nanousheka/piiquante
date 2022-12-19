5e950c56313bb8e3a0547014162f39e4dcf6509c
nanousheka <ineszocly@gmail.com>
2022-12-19 07:21:40 -0400

    Re-install frontend

--- backend/app.js
+++ backend/app.js
@@ -19,11 +19,11 @@ useUnifiedTopology: true })
 
 //Helps preventing cross-origins errors.
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
 
 app.use(bodyParser.json());
